52dff064211a7538484a6bfa68191441ff6c19aa
Philip Nelson <philipnelson5@gmail.com>
2019-04-29 12:19:51 -0600

    square root example

---
+++ Sqrt/sqrt1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "sqrt1.hpp"
+
+int main() {
+  std::cout << "Sqrt<16>::value = " << Sqrt<16>::value << '\n';
+  std::cout << "Sqrt<25>::value = " << Sqrt<25>::value << '\n';
+  std::cout << "Sqrt<42>::value = " << Sqrt<42>::value << '\n';
+  std::cout << "Sqrt<1>::value = " << Sqrt<1>::value << '\n';
+  std::cout << "Sqrt<1>::value = " << Sqrt<2>::value << '\n';
+}
+
